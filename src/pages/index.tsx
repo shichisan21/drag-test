4bd291cf9a54c1648a52f837df81d2f6157a8fea
shichisan21 <photograph.by@gmail.com>
2023-04-14 23:56:46 +0900

    パネルレイアウト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import DragComponent from "@/components/DragComponent";
 import SideMenu from "@/components/SideMenu";
+import { Grid, Paper, Typography } from "@mui/material";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,33 @@ export default function Home(): ReactElement {
       <SideMenu />
       <main className={styles.main}>
         <DragComponent />
+        <Grid container>
+          <Grid item xs={10}>
+            <Typography variant='h6' gutterBottom component='div'>
+              Grid with nested items
+            </Typography>
+            <Grid container spacing={2}>
+              {[0, 1, 2].map((value) => (
+                <Grid key={value} item alignItems={"center"}>
+                  <Paper
+                    sx={{ p: 2, display: "flex", flexDirection: "row" }}
+                    elevation={0}
+                  >
+                    <Typography variant='h6' component='div'>
+                      {value}
+                    </Typography>
+                    <Typography variant='body2'>
+                      {`Cras mattis consectetur purus sit amet fermentum.
+                    Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+                    Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                    Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`}
+                    </Typography>
+                  </Paper>
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
       </main>
     </div>
   );
