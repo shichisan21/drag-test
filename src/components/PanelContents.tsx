cdd12d1a80a1e40026061279bccf1855d34b50b6
shichisan21 <photograph.by@gmail.com>
2023-04-15 01:49:31 +0900

    パネル修正

---
+++ src/components/PanelContents.tsx
@@ -0,0 +1,47 @@
+import { Box, Grid, Paper, Typography } from "@mui/material";
+import { InfoOutlined } from "@mui/icons-material";
+
+interface Props {
+  value: string;
+}
+export const PanelContents = ({ value }: Props) => {
+  return (
+    <Grid key={value} item xs={4} alignItems={"stretch"}>
+      <Paper
+        sx={{
+          p: 2,
+          display: "flex",
+          flexDirection: "row",
+          bgcolor: "lightblue",
+        }}
+        elevation={0}
+      >
+        <Typography variant='body2'>
+          <InfoOutlined sx={{ marginRight: 1 }} />
+          <Typography variant='h6' component='span' justifyContent={"center"}>
+            {value}
+          </Typography>
+          <br />
+          {`Cras mattis consectetur purus sit amet fermentum.
+          Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+          Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+          Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`}
+        </Typography>
+      </Paper>
+      <Box
+        sx={{
+          height: 40,
+          width: "100%",
+          bgcolor: "#cfe8fc",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Typography variant='body2' textAlign='center'>
+          aaaa
+        </Typography>
+      </Box>
+    </Grid>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
 import { ReactElement } from "react";
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import DragComponent from "@/components/DragComponent";
 import SideMenu from "@/components/SideMenu";
-import { Grid, Paper, Typography } from "@mui/material";
+import { PanelContents } from "@/components/PanelContents";
+import { Grid } from "@mui/material";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home(): ReactElement {
+  const fruits = ["apple", "banana", "orange"];
   return (
     <div>
       <Head>
@@ -22,23 +23,8 @@ export default function Home(): ReactElement {
       <main className={styles.main}>
         <DragComponent />
         <Grid container spacing={2}>
-          {[0, 1, 2].map((value) => (
-            <Grid key={value} item xs={4} alignItems={"center"}>
-              <Paper
-                sx={{ p: 2, display: "flex", flexDirection: "row" }}
-                elevation={0}
-              >
-                <Typography variant='h6' component='div'>
-                  {value}
-                </Typography>
-                <Typography variant='body2'>
-                  {`Cras mattis consectetur purus sit amet fermentum.
-                    Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-                    Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-                    Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`}
-                </Typography>
-              </Paper>
-            </Grid>
+          {fruits.map((value) => (
+            <PanelContents key={value} value={value} />
           ))}
         </Grid>
       </main>
