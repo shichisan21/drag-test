1fb747be2ec3a664e3d355a7dde0aa0c4c642512
shichisan21 <photograph.by@gmail.com>
2023-04-25 22:46:48 +0900

    List

--- src/pages/listPage.tsx
+++ src/pages/listPage.tsx
@@ -1,19 +1,26 @@
 import { FC, ReactElement } from "react";
 import { useRouter } from "next/router";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "name", headerName: "Name", width: 130 },
+];
 
 interface ListPageProps {
   fruits: string[];
 }
 
 const ListPage: FC<ListPageProps> = ({ fruits }): ReactElement => {
+  const rows = fruits
+    ? fruits.map((fruit, index) => ({
+        id: index,
+        name: fruit,
+      }))
+    : [];
   return (
-    <div>
-      <h1>test List</h1>
-      <ul>
-        {fruits.map((fruit) => (
-          <li key={fruit}>{fruit}</li>
-        ))}
-      </ul>
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid rows={rows} columns={columns} />
     </div>
   );
 };
@@ -21,6 +28,10 @@ const ListPage: FC<ListPageProps> = ({ fruits }): ReactElement => {
 const ListPageWrapper: FC = (): ReactElement => {
   const router = useRouter();
   const { fruits } = router.query;
+  if (!fruits) {
+    return <></>;
+  }
+
   return <ListPage fruits={fruits as string[]} />;
 };
 
