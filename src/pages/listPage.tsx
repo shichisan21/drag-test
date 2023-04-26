d0abdedcecfb501c84513107742e90cccf6b635e
shichisan21 <photograph.by@gmail.com>
2023-04-27 00:06:28 +0900

    リスト

--- src/pages/listPage.tsx
+++ src/pages/listPage.tsx
@@ -5,6 +5,8 @@ import { DataGrid, GridColDef } from "@mui/x-data-grid";
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 70 },
   { field: "name", headerName: "Name", width: 130 },
+  { field: "price", headerName: "Price", width: 90 },
+  { field: "description", headerName: "Description", width: 500 },
 ];
 
 interface ListPageProps {
@@ -16,10 +18,12 @@ const ListPage: FC<ListPageProps> = ({ fruits }): ReactElement => {
     ? fruits.map((fruit, index) => ({
         id: index,
         name: fruit,
+        price: Math.floor(Math.random() * 1000) + 1,
+        description: `A delicious ${fruit} that is great for snacking.`,
       }))
     : [];
   return (
-    <div style={{ height: 400, width: "100%" }}>
+    <div style={{ height: 900, width: "100%" }}>
       <DataGrid rows={rows} columns={columns} />
     </div>
   );
