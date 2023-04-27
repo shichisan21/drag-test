80f49fe67f43c59ae133eb6d140040dc1e0d3069
shichisan21 <photograph.by@gmail.com>
2023-04-27 22:15:07 +0900

    タイムスタンプ

--- src/pages/dragAndDrop.tsx
+++ src/pages/dragAndDrop.tsx
@@ -6,19 +6,22 @@ interface DragAndDropProps {
 }
 
 const DragAndDrop: FC<DragAndDropProps> = ({ fruits }): ReactElement => {
-  const [isDropped, setIsDropped] = useState(false);
+  const [isDropped, setIsDropped] = useState<boolean>(false);
   const [fileContent, setFileContent] = useState<string>();
+  const [dropTimeStamp, setDropTimeStamp] = useState<number>();
 
-  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>): void => {
     event.preventDefault();
     const file = event.dataTransfer.files[0];
     if (file.type === "text/csv") {
       const reader = new FileReader();
-      reader.onload = (event) => {
+      reader.onload = (event: ProgressEvent<FileReader>) => {
         const text = event.target?.result;
         setFileContent(text as string);
       };
       reader.readAsText(file);
+      const timestamp = new Date().getTime();
+      setDropTimeStamp(timestamp);
 
       setIsDropped(true); // <- ここで状態を更新する
     }
@@ -30,8 +33,13 @@ const DragAndDrop: FC<DragAndDropProps> = ({ fruits }): ReactElement => {
         <h1>testDragAndDrop</h1>
       </div>
       <div onDrop={handleDrop} onDragOver={(event) => event.preventDefault()}>
-        <h1>test sssAbout</h1>
-        {fileContent && <pre>{fileContent}</pre>}
+        {fileContent && (
+          <>
+            <p>File Content: </p>
+            <pre>{fileContent}</pre>
+            <p>Drop TimeStamp: {dropTimeStamp}</p>
+          </>
+        )}
         <ul>
           {fruits.map((fruit) => (
             <li key={fruit}>{fruit}</li>
