d31e07e2f6e4f7e662a2ab639743790be944706f
shichisan21 <photograph.by@gmail.com>
2023-05-04 01:29:35 +0900

    ファイル分割

--- src/pages/dragAndDrop.tsx
+++ src/pages/dragAndDrop.tsx
@@ -1,45 +1,21 @@
 import { FC, ReactElement, useState } from "react";
 import { useRouter } from "next/router";
 import { Box, Grid, Typography } from "@mui/material";
-import { handleDragEnter, handleDragLeave } from "@/utils/dragAndDropHandlers";
+import {
+  handleDragEnter,
+  handleDragLeave,
+  handleDrop,
+} from "@/utils/dragAndDropHandlers";
 
 interface DragAndDropProps {
   fruits: string[];
 }
 
 const DragAndDrop: FC<DragAndDropProps> = ({ fruits }): ReactElement => {
-  const [isDropped, setIsDropped] = useState<boolean>(false);
-  const [fileContent, setFileContent] = useState<string>();
-  const [dropTimeStamp, setDropTimeStamp] = useState<string>();
-  const [droppedBoxId, setDroppedBoxId] = useState<string>();
+  const [fileContent, setFileContent] = useState<string>("");
+  const [dropTimeStamp, setDropTimeStamp] = useState<string>("");
+  const [droppedBoxId, setDroppedBoxId] = useState<string>("");
   const [highlightedBoxId, setHighlightedBoxId] = useState<string>("");
-
-  const handleDrop = (
-    event: React.DragEvent<HTMLDivElement>,
-    boxId: string
-  ): void => {
-    event.preventDefault();
-    const file = event.dataTransfer.files[0];
-    if (file.type === "text/csv") {
-      const reader = new FileReader();
-      reader.onload = async (event: ProgressEvent<FileReader>) => {
-        const text = event.target?.result;
-        if (/\<script\>.*\<\/script\>/i.test(text as string)) {
-          alert("Script is not allowed");
-        } else {
-          setFileContent(text as string);
-        }
-      };
-      reader.readAsText(file);
-      const timestamp = new Date().getTime();
-      setDropTimeStamp(
-        new Date(timestamp).toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" })
-      );
-
-      setIsDropped(true); // <- ここで状態を更新する
-      setDroppedBoxId(boxId);
-    }
-  };
 
   return (
     <>
@@ -81,7 +57,15 @@ const DragAndDrop: FC<DragAndDropProps> = ({ fruits }): ReactElement => {
                       ? "lightgray"
                       : undefined,
                 }}
-                onDrop={(event) => handleDrop(event, `Box-No.${index + 1}`)}
+                onDrop={(event) =>
+                  handleDrop(
+                    event,
+                    `Box-No.${index + 1}`,
+                    setFileContent,
+                    setDropTimeStamp,
+                    setDroppedBoxId
+                  )
+                }
                 onDragEnter={(event) =>
                   handleDragEnter(
                     event,
--- src/utils/dragAndDropHandlers.ts
+++ src/utils/dragAndDropHandlers.ts
@@ -16,3 +16,32 @@ export const handleDragLeave = (
   event.stopPropagation();
   setHighlightedBoxId("");
 };
+
+export const handleDrop = (
+  event: React.DragEvent<HTMLDivElement>,
+  boxId: string,
+  setFileContent: Dispatch<SetStateAction<string>>,
+  setDropTimeStamp: Dispatch<SetStateAction<string>>,
+  setDroppedBoxId: Dispatch<SetStateAction<string>>
+): void => {
+  event.preventDefault();
+  const file = event.dataTransfer.files[0];
+  if (file.type === "text/csv") {
+    const reader = new FileReader();
+    reader.onload = async (event: ProgressEvent<FileReader>) => {
+      const text = event.target?.result;
+      if (/\<script\>.*\<\/script\>/i.test(text as string)) {
+        alert("Script is not allowed");
+      } else {
+        setFileContent(text as string);
+      }
+    };
+    reader.readAsText(file);
+    const timestamp = new Date().getTime();
+    setDropTimeStamp(
+      new Date(timestamp).toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" })
+    );
+
+    setDroppedBoxId(boxId);
+  }
+};
