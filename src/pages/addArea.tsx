ec1f5bc694527a4fcafc210313e504679baa3379
shichisan21 <photograph.by@gmail.com>
2023-05-07 01:10:47 +0900

    状態管理

--- src/pages/addArea.tsx
+++ src/pages/addArea.tsx
@@ -2,10 +2,14 @@ import { FC, ReactElement, useState } from "react";
 import { useRouter } from "next/router";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
 interface ButtonState {
   label: string;
   isExtended: boolean;
+  selectedValue?: string;
 }
 
 const AddArea: FC<{
@@ -34,6 +38,12 @@ const MyComponent: FC = (): ReactElement => {
     setButtons(newButtons);
   };
 
+  const handleRadioChange = (index: number, value: string) => {
+    const newButtons = [...buttons];
+    newButtons[index].selectedValue = value;
+    setButtons(newButtons);
+  };
+
   return (
     <Box display='flex' flexDirection='column'>
       {buttons.map((button, index) => (
@@ -46,10 +56,40 @@ const MyComponent: FC = (): ReactElement => {
           {button.isExtended && (
             <Box sx={{ backgroundColor: "lightblue", color: "white" }}>
               <p>Extendedエリア({button.label})</p>
+              <RadioGroup
+                value={button.selectedValue}
+                onChange={(event) =>
+                  handleRadioChange(index, event.target.value)
+                }
+              >
+                <FormControlLabel
+                  value='option1'
+                  control={<Radio />}
+                  label='Option 1'
+                />
+                <FormControlLabel
+                  value='option2'
+                  control={<Radio />}
+                  label='Option 2'
+                />
+                <FormControlLabel
+                  value='option3'
+                  control={<Radio />}
+                  label='Option 3'
+                />
+              </RadioGroup>
             </Box>
           )}
         </div>
       ))}
+      <Box mt={2}>
+        <p>選択されたラジオボタン:</p>
+        {buttons.map((button, index) => (
+          <p key={index}>
+            {button.label}: {button.selectedValue || "未選択"}
+          </p>
+        ))}
+      </Box>
     </Box>
   );
 };
