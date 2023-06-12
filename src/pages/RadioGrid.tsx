8d0d2a838bb042fc1d03515664624b0ada38d08f
shichisan21 <photograph.by@gmail.com>
2023-06-13 00:12:51 +0900

    初期値

--- src/pages/RadioGrid.tsx
+++ src/pages/RadioGrid.tsx
@@ -2,71 +2,56 @@ import { FC, ReactElement, useState } from "react";
 import { Container, Typography } from "@mui/material";
 import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 import { Radio, RadioGroup, FormControlLabel } from "@mui/material";
-import { useRouter } from "next/router";
 
 interface DataRow {
   id: number;
   name: string;
+  nameId: number;
   group: string;
-  [key: string]: string | number;
+  groupId: number;
+}
+
+interface GroupRow {
+  groupId: number;
+  group: string;
 }
 
 const RadioGrid: FC = (): ReactElement => {
-  const [selectedValues, setSelectedValues] = useState<{
-    [key: string]: string;
-  }>({});
-  const router = useRouter();
+  let data = {
+    arr1: [
+      { id: 1, name: "John", nameId: 1, group: "Group A", groupId: 1 },
+      { id: 2, name: "Alice", nameId: 2, group: "Group B", groupId: 2 },
+      { id: 3, name: "Bob", nameId: 3, group: "Group A", groupId: 1 },
+      { id: 4, name: "Eve", nameId: 4, group: "Group C", groupId: 3 },
+    ],
+  };
 
-  let dataArray: DataRow[] = [
-    { id: 1, name: "John", group: "Group A", age: 25, status: "Active" },
-    { id: 2, name: "Alice", group: "Group B", age: 30, status: "Inactive" },
-    { id: 3, name: "Bob", group: "Group A", age: 35, status: "Active" },
-    { id: 4, name: "Eve", group: "Group B", age: 28, status: "Active" },
-  ];
-
-  const additionalColumns: GridColDef[] =
-    dataArray && dataArray.length > 0
-      ? Object.keys(dataArray[0])
-          .filter((key) => !["group", "name"].includes(key))
-          .map((key) => ({
-            field: key,
-            headerName: key,
-            width: 200,
-            renderCell: (params: GridRenderCellParams<DataRow>) => {
-              return (
-                <RadioGroup
-                  value={selectedValues[key] || ""}
-                  onChange={(event) => {
-                    setSelectedValues((prevSelectedValues) => ({
-                      ...prevSelectedValues,
-                      [key]: event.target.value,
-                    }));
-                  }}
-                >
-                  <FormControlLabel
-                    value={params.row.id.toString()}
-                    control={<Radio />}
-                  />
-                </RadioGroup>
-              );
-            },
-          }))
-      : [];
+  // Set initial state of the radio buttons according to the group value of each row
+  const [selectedValues, setSelectedValues] = useState(
+    data.arr1.reduce((acc, row) => {
+      acc[row.name] = row.id.toString();
+      return acc;
+    }, {})
+  );
 
   const columns: GridColDef[] = [
-    { field: "group", headerName: "Group", width: 200 },
     {
-      field: "name",
-      headerName: "Name",
+      field: "group",
+      headerName: "Group",
+      width: 200,
+    },
+    ...data.arr1.map((row: DataRow) => ({
+      field: row.name,
+      headerName: row.name,
       width: 200,
       renderCell: (params: GridRenderCellParams<DataRow>) => {
         return (
           <RadioGroup
-            value={selectedValues["name"] || ""}
+            value={selectedValues[row.name] || ""}
             onChange={(event) => {
               setSelectedValues((prevSelectedValues) => ({
                 ...prevSelectedValues,
-                name: event.target.value,
+                [row.name]: event.target.value,
               }));
             }}
           >
@@ -77,25 +62,22 @@ const RadioGrid: FC = (): ReactElement => {
           </RadioGroup>
         );
       },
-    },
-    ...additionalColumns,
+    })),
   ];
 
-  const getSelectedRowGroup = (id: string) => {
-    const selectedRow = dataArray.find((row) => row.id.toString() === id);
-    return selectedRow ? selectedRow.group : "No selection";
-  };
   return (
     <>
       <Container>
         <Typography variant='h6' gutterBottom>
-          Selected Groups:
-          {Object.values(selectedValues).map((value, index) => (
-            <Typography key={index}>{getSelectedRowGroup(value)}</Typography>
+          Selected Values:
+          {Object.entries(selectedValues).map(([key, value], index) => (
+            <Typography key={index}>
+              {key}: {data.arr1.find((row) => row.id === Number(value))?.group}
+            </Typography>
           ))}
         </Typography>
         <DataGrid<DataRow>
-          rows={dataArray}
+          rows={data.arr1}
           columns={columns}
           sx={{ height: 900 }}
         />
